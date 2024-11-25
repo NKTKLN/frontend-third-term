05ea33647b0ad2d7be9dc5d7eeb41fe5a7f15a38
NKTKLN <nktkln@nktkln.com>
2024-11-25 13:31:38 +0300

    Update services.js

--- src/js/services.js
+++ src/js/services.js
@@ -1,26 +1,3 @@
-// 11 practice
-function practice_11() {
-  const h1 = document.querySelector('h1');
-  if (h1) {
-    h1.textContent = 'Добро пожаловать на наш сайт!';
-  }
-
-  const h2 = document.querySelector('h2');
-  if (h2) {
-    h2.style.color = 'red';
-  }
-
-  const firstParagraph = document.querySelector('p');
-  if (firstParagraph) {
-    firstParagraph.textContent = 'Это новый текст параграфа.';
-  }
-
-  const video = document.querySelector('video');
-  if (video) {
-    video.style.display = 'none';
-  }
-}
-
 // Modal
 // Получаем модальное окно и кнопки открытия
 const modal = document.getElementById("modal");
